c7f90673395cdd3a5044d932e15f3b925c52e7b9
Parth <Parthpurbia@gmail.com>
2025-05-23 19:20:15 +0530

    Modification

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@
  TARGET_DIR=$1
  echo "Cleaning up .log files older than 2 days in: $TARGET_DIR"
 
-find "$TARGET_DIR" -type f -name "*.log" -mtime +2 -print -delere
+find "$TARGET_DIR" -type f -name "*.log" -print -delere
